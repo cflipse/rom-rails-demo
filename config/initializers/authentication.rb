61b3e106cbf63246520b58ff3979b7ab5f6abafe
Chris Flipse <cflipse@digitalocean.com>
2018-04-23 09:17:05 -0400

    configure Warden to pull user info from authentication

--- config/initializers/authentication.rb
+++ config/initializers/authentication.rb
@@ -3,3 +3,37 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
 end
+
+Rails.application.config.middleware.use Warden::Manager do |manager|
+  manager.default_strategies :omniauth
+  manager.failure_app = ->(env) { SessionsController.action(:new).call(env) }
+
+  manager.serialize_into_session do |user|
+    [user.provider, user.uid, user.info]
+  end
+
+  manager.serialize_from_session do |data|
+    OpenStruct.new(data.last)
+  end
+end
+
+Warden::Strategies.add(:omniauth) do
+  def omniauth
+    request.env["omniauth.auth"]
+  end
+
+  def valid?
+    omniauth.present?
+  end
+
+  # rubocop:disable Style/SignalException
+  #   fail has been overriden with a specific, non-exception meaning for warden
+  def authenticate!
+    if omniauth.info.email.blank?
+      fail "no email found!"
+    else
+      success! omniauth
+    end
+  end
+  # rubocop:enable Style/SignalException
+end
