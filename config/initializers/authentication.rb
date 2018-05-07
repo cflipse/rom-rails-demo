76aa6d2d2d06f78bfeb24e758f88a706bb399c93
Chris Flipse <cflipse@gmail.com>
2018-05-07 13:50:31 -0400

    add google auth configuration

--- config/initializers/authentication.rb
+++ config/initializers/authentication.rb
@@ -2,6 +2,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
 
 Rails.application.config.middleware.use Warden::Manager do |manager|
